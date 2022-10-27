84b578aa81576293ebd2bc4d2ba29ca0572074c1
afzal75 <afzalbhuiyan16@gmail.com>
2022-10-27 21:42:48 +0600

    faq section done

--- src/Pages/FAQ/FAQ/FAQ.jsx
+++ src/Pages/FAQ/FAQ/FAQ.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-        <div>
-            <h2>This is FAQ</h2>
+        <div style={{width: "50%", margin: "0 auto"}}>
+            <h3>Website Name: Programming Education.</h3>
+            <h4>
+                <h3>Features:</h3>
+                <ul>
+                    <ol>1. Clickeable Course Card</ol>
+                    <ol>2. Firebase Authentication</ol>
+                    <ol>3. Mobile and desktop responsive</ol>
+                    <ol>4. Toggole Button ( Open & Close)</ol>
+                    <ol>5. CheakeOut Page.</ol>
+                    <ol>6. Private route</ol>
+                </ul>
+                Techonologies:
+                <br/>
+                 Client side: React, React-Bootstrap, React Icons, React-router-dom, Firebase.
+                 <br/>
+                 Server side: Node, Express, Vercel
+            </h4>
         </div>
     );
 };
